0ad6f7fa86d8b3acbcaea1d31ec723dee3d5e66c
syarifh-id <125syarifh@gmail.com>
2024-01-17 14:28:32 +0700

    basic data type

---
+++ data-science/basic.py
@@ -0,0 +1,13 @@
+nama = "syarifh"
+namaBelakang = 'hidayat'
+print("nama saya adalah",nama,namaBelakang)
+
+usia = 24
+print("umur saya",usia)
+
+tinggi = 1.8
+print("tinggi saya adalah",tinggi,"meter")
+
+hobby = ["membaca", "koding", "olahraga" ]
+print("Hobby saya yang saya sedang tekuni adalah",hobby[1])
+
