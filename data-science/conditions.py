88bd30c4a2b894edbc83e9ae04c7f9ff3d960c3e
syarifh-id <125syarifh@gmail.com>
2024-01-19 19:55:18 +0700

    if else elif

--- data-science/conditions.py
+++ data-science/conditions.py
@@ -1,6 +1,16 @@
-i = int(input("Masukkan Angka :"))
+# i = int(input("Masukkan Angka :4"))
 
-if i%2 == 0:
-	print('bilangan genap')
-else :
-	print('bilangang ganjil')
+# if i%2 == 0:
+# 	print('bilangan genap')
+# else :
+# 	print('bilangan ganjil')
+
+
+nilai =int(input("masukkan nilai anda :"))
+
+if nilai >= 90 :
+	print("nilai A")
+elif 90 > nilai >=65 :
+	print("nilai anda B")
+else:
+	print("nilai anda C") 
