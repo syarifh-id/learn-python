368040fac6db289747449078b43534fa0298347b
syarifh-id <125syarifh@gmail.com>
2024-01-21 23:23:31 +0700

    casting variable

--- data-science/conditions.py
+++ data-science/conditions.py
@@ -1,4 +1,13 @@
+
+# Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+    # int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+    # float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+    # str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
+
 i = int(input("Masukkan Angka :"))
+
+
 
 if i%2 == 0:
 	print('bilangan genap')
