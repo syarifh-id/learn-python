cb343966fe48cec571a9f384b86dafe05b1364cf
syarifh-id <125syarifh@gmail.com>
2024-01-22 22:45:21 +0700

    add some explains

--- data-science/basic.py
+++ data-science/basic.py
@@ -8,6 +8,18 @@ print("umur saya",usia)
 #float
 tinggi = 1.8
 print("tinggi saya adalah",tinggi,"meter")
+
+
+
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+
+
+
 #list *mutable
 hobby = ["membaca", "koding", "olahraga" ]
 print("Hobby saya yang saya sedang tekuni adalah",hobby[1])
