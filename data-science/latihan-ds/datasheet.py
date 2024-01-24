47a2b568fd9e7865ae48b3fb13a157d3b9f39f68
syarifh-id <125syarifh@gmail.com>
2024-01-24 07:50:56 +0700

    data extends

--- data-science/latihan-ds/datasheet.py
+++ data-science/latihan-ds/datasheet.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as pylot
 import seaborn as sns
 
 dataPath = 'D:\my-code\learn-python\data-science\latihan-ds\mobil.csv'
-
 data = pd.read_csv(dataPath)
 
 # print(data)
@@ -13,3 +12,19 @@ mobilTersedikit = data['Merek Mobil'].value_counts().idxmin()
 
 print('Mobil dengan penjualan terbanyak adalah :', mobilTerbanyak)
 print('Mobil dengan penjualan paling sedikit adalah :', mobilTersedikit)
+
+hondaTerbanyak = data[data['Merek Mobil'] == 'Honda'].value_counts().idxmax()
+hondaTerbanyak = data[data['Merek Mobil'] == 'Toyota'].value_counts().idxmax()
+
+print(hondaTerbanyak)
+
+bulanTerbanyak = data['Bulan'].value_counts().nlargest(10).index.tolist()
+
+print(bulanTerbanyak)
+
+data['Harga'] = data['Harga'].str.replace('[Rp.]', '', regex=True).str.replace('[.]', '', regex=True).astype(int)
+data['Pendapatan'] = data['Mobil Terjual'] * data['Harga']
+
+bulanPendapatanTerbanyak = data.groupby('Bulan')['Pendapatan'].sum().idxmax()
+
+print(bulanPendapatanTerbanyak)
