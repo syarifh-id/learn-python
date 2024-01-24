92b4e89db328bc239e014fcbdee3a73e7945343e
syarifh-id <125syarifh@gmail.com>
2024-01-24 20:48:51 +0700

    add explains

--- data-science/latihan-ds/tempCodeRunnerFile.py
+++ data-science/latihan-ds/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-matplotlib.pylot
+data['Harga'] = data['Harga'].str.replace('[Rp.]', '', regex=True).str.replace('[.]', '', regex=True).astype(int)
+data['Pendapatan'] = data['Mobil Terjual'] * data['Harga']
