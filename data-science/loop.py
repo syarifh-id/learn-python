a68407542a5a659d6069712acfee89627e712ac7
syarifh-id <125syarifh@gmail.com>
2024-01-27 22:17:46 +0700

    fol loop

--- data-science/loop.py
+++ data-science/loop.py
@@ -13,3 +13,29 @@ while i < 6:
   if i == 3:
     break
   i += 1
+  
+  
+#loop use for
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "banana":
+  print(x)
+  
+  
+#and also can use a break keyword
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+	print(x)
+	if x == "banana":
+		break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
