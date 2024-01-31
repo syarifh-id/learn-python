e86986e42d321f395002c6474acc9e3795e6a0a1
syarifh-id <125syarifh@gmail.com>
2024-01-31 07:54:39 +0700

    pandas coba coba

---
+++ data-science/latihan-ds/prosessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+data = pd.read_csv('D:\my-code\learn-python\data-science\latihan-ds\mobil.csv')
+
+
+x = data['Bulan'].lengt()
+
+print(x)
