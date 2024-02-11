8bc8c4026ab33a7dd7d408080a054b7518e729e0
syarifh-id <125syarifh@gmail.com>
2024-02-11 22:07:47 +0700

    pyplot indeept

--- data-science/latihan-ds/visualisasi-2.py
+++ data-science/latihan-ds/visualisasi-2.py
@@ -1,13 +1,16 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 import seaborn as sb
 
 dataPath = "D:\my-code\learn-python\data-science\latihan-ds\csv\kemiskinan.csv"
 
-data = pd.read_csv(dataPath)
+df = pd.read_csv(dataPath)
 
-plt.bar(data['Provinsi'], data['Penduduk'])
-plt.xlabel('Provinsi')
-plt.ylabel('Penduduk')
-plt.xticks(rotation=90)
-plt.show()
+
+
+# print(df)
+# plot.bar(df['Provinsi'], df['Penduduk'])
+# plot.xlabel('Provinsi')
+# plot.ylabel('Penduduk')
+# plot.xticks(rotation=15)
+# plot.show()
