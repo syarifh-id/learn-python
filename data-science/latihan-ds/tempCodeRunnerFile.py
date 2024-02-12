f73b0eab609392a65c7e29ef085f70c1a6a6541a
syarifh-id <125syarifh@gmail.com>
2024-02-12 17:57:04 +0700

    add some function

--- data-science/latihan-ds/tempCodeRunnerFile.py
+++ data-science/latihan-ds/tempCodeRunnerFile.py
@@ -1,2 +1,15 @@
-data['Harga'] = data['Harga'].str.replace('[Rp.]', '', regex=True).str.replace('[.]', '', regex=True).astype(int)
-data['Pendapatan'] = data['Mobil Terjual'] * data['Harga']
+import pandas as pd
+import matplotlib.pyplot as plot
+import seaborn as sb
+
+dataPath = "D:\my-code\learn-python\data-science\latihan-ds\csv\kemiskinan.csv"
+
+df = pd.read_csv(dataPath)
+#print(df.columns)
+# print(df)
+
+plot.bar(df['Provinsi'], df['Kemiskinan'])
+plot.xlabel('Provinsi')
+plot.ylabel('Penduduk')
+plot.xticks(rotation=45)
+plot.show()
--- data-science/latihan-ds/visualisasi-2.py
+++ data-science/latihan-ds/visualisasi-2.py
@@ -5,12 +5,11 @@ import seaborn as sb
 dataPath = "D:\my-code\learn-python\data-science\latihan-ds\csv\kemiskinan.csv"
 
 df = pd.read_csv(dataPath)
-
-
-
+#print(df.columns)
 # print(df)
-# plot.bar(df['Provinsi'], df['Penduduk'])
-# plot.xlabel('Provinsi')
-# plot.ylabel('Penduduk')
-# plot.xticks(rotation=15)
-# plot.show()
+
+plot.bar(df['Provinsi'], df['Kepadatan'])
+plot.xlabel('Provinsi')
+plot.ylabel('Penduduk')
+plot.xticks(rotation=90)
+plot.show()
