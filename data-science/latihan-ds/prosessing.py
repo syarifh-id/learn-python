042b191e48892c38925bd4d8bab1b40660fe99af
syarifh-id <125syarifh@gmail.com>
2024-02-18 09:10:11 +0700

    fix errors

--- data-science/latihan-ds/prosessing.py
+++ data-science/latihan-ds/prosessing.py
@@ -3,9 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 dataPath = "D:\my-code\learn-python\data-science\latihan-ds\csv\penjualan_mobil.csv"
-data2 = 'D:\my-code\learn-python\data-science\ITTS-DS\spotify-2023.csv'
 #read data 
-data = pd.read_csv(data2)
+data = pd.read_csv(dataPath)
 print(data)
 #penjualan terbanyak 
 mostSales = data['Penjualan'].value_counts().idxmax()
